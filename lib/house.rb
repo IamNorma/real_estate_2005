b37f37c48b29ff4648540bd7aff1b70dde5c32f9
Norma Lopez <norma_l304@yahoo.com>
2020-05-13 09:29:06 -0600

    Add rooms_from_category method with passing test

--- lib/house.rb
+++ lib/house.rb
@@ -14,4 +14,10 @@ class House
   def above_market_average?
     @price > 500_000
   end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      category == room.category
+    end
+  end
 end
