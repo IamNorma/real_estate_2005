3366822fb215cbe788c1919cdf954a6c8e7d29b3
Norma Lopez <norma_l304@yahoo.com>
2020-05-13 09:16:50 -0600

    Add attr_readers for price and address with passing test

--- test/house_test.rb
+++ test/house_test.rb
@@ -7,6 +7,13 @@ class HouseTest < Minitest::Test
   def test_it_exists
     house = House.new("$400000", "123 sugar lane")
 
-    assert_instance_of House, house 
+    assert_instance_of House, house
+  end
+
+  def test_it_has_readable_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal 400_000, house.price
+    assert_equal "123 sugar lane", house.address
   end
 end
