e488c2c32857df11437fb6856811dfe1c2af2346
Norma Lopez <norma_l304@yahoo.com>
2020-05-13 09:54:45 -0600

    Add expected variable with value of hash for rooms_by_category_test

--- test/house_test.rb
+++ test/house_test.rb
@@ -115,6 +115,8 @@ class HouseTest < Minitest::Test
     house.add_room(room_3)
     house.add_room(room_2)
 
-    assert_equal ({:bedroom=>[room_1, room_2], :living_room=> [room_3], :basement=> [room_4]}), house.rooms_by_category
+    expected = {:bedroom=>[room_1, room_2], :living_room=> [room_3], :basement=> [room_4]}
+
+    assert_equal expected, house.rooms_by_category
   end
 end
